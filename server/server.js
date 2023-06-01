b447aca262975929ffa134e608f9d060eca407b9
Juan Brizuela <73278498+brizuela-go@users.noreply.github.com>
2023-06-01 02:40:13 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,16 +24,15 @@ app.get("/", async (req, res) => {
 app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
-
-    const response = await openai.createCompletion({
-      model: "text-davinci-003", // gpt-3 model
-      prompt: `${prompt}`,
-      temperature: 0.35, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 1, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    
+    // gpt-3.5-turbo
+    // replace .createCompletion() with .createChatCompletion()
+    const completion = await openai.createChatCompletion({ 
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: `${prompt}`}], 
     });
+
+
 
     res.status(200).send({
       bot: response.data.choices[0].text,
